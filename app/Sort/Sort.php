d1b12ba4e977786a6b964e257f3c02f935d641ee
Modavid <modavid096@gmail.com>
2017-01-11 16:46:23 +0100

    Tri Selection

--- app/Sort/Sort.php
+++ app/Sort/Sort.php
@@ -11,15 +11,31 @@ namespace App\Sort;
 
 class Sort
 {
-    public function Bubble($tab){
+    public function Bubble($tab)
+    {
 
     }
 
-    public function Insertion($tab){
+    public function Insertion($tab)
+    {
 
     }
 
-    public function Selection($tab){
-
+    public function Selection($tab)
+    {
+        $n = 0;
+        $size = sizeof($tab);
+        for ($i = 0; $i < $size; $i++) {
+            $n++;
+            for ($j = $i + 1; $j < $size; $j++) {
+                if ($tab[$i] > $tab[$j]) {
+                    $tmp = $tab[$i];
+                    $tab[$i] = $tab[$j];
+                    $tab[$j] = $tmp;
+                }
+            }
+        }
+        $array = array("tab" => $tab, 'cost' => $n);
+        return $array;
     }
 }
--- pages/home.php
+++ pages/home.php
@@ -1,3 +1,11 @@
 <?php
-    use App\Tab;
+use App\Sort\Sort;
+use App\Tab;
 
+
+$sort = new Sort();
+
+var_dump($sort->Selection(Tab::getTab(10)));
+Tab::getTab(10,'reverse');
+Tab::getTab(10,'nearlySorted');
+Tab::getTab(10,'random');
